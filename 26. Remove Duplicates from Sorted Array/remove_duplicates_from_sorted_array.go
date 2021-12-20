e83315731362dad8090ccc575cdf8d003665e3a5
Daman <aroradaman@gmail.com>
2021-12-20 23:49:49 +0530

    Remove Duplicates from Sorted Arra

---
+++ 26. Remove Duplicates from Sorted Array/remove_duplicates_from_sorted_array.go
@@ -0,0 +1,21 @@
+package main
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array
+
+func removeDuplicates(nums []int) int {
+
+	if len(nums) == 0 {
+		return 0
+	}
+
+	var slow, fast int
+
+	for fast = 1; fast < len(nums); fast += 1 {
+		if nums[slow] != nums[fast] {
+			slow += 1
+			nums[slow] = nums[fast]
+		}
+	}
+
+	return slow + 1
+}
