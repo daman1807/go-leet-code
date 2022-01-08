0d3c1382971be2b02aec9265e6953689643e9985
Daman <aroradaman@gmail.com>
2022-01-08 22:07:14 +0530

    missing-number

--- 268. Missing Number/missing-number.go
+++ 268. Missing Number/missing-number.go
@@ -3,6 +3,7 @@ package main
 // https://leetcode.com/problems/missing-number/
 
 func missingNumber(nums []int) int {
+
 	res := len(nums) * (len(nums) + 1) / 2
 	for i := 0; i < len(nums); i++ {
 		res -= nums[i]
