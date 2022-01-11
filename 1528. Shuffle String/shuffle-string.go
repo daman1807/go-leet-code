5bce27d70ce0376f1f8fde8205bdab4fe0e5d04a
Daman <aroradaman@gmail.com>
2022-01-11 23:39:28 +0530

    shuffle-string

---
+++ 1528. Shuffle String/shuffle-string.go
@@ -0,0 +1,12 @@
+package main
+
+// https://leetcode.com/problems/shuffle-string/
+
+func restoreString(s string, indices []int) string {
+	res := make([]byte, len(s))
+
+	for i:=0; i<len(indices); i++{
+		res[indices[i]] = s[i]
+	}
+	return string(res)
+}
