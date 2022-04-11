6743ff4e3bce6d3b1f551b6bd300999034984474
Daman <aroradaman@gmail.com>
2022-04-11 18:15:12 +0530

    integer-to-roman

--- 12. Integer to Roman/integer-to-roman.go
+++ 12. Integer to Roman/integer-to-roman.go
@@ -2,17 +2,37 @@ package main
 
 // https://leetcode.com/problems/integer-to-roman/
 
-func intToRoman(num int) (s string) {
-	literals := [...]string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
-	values := [...]int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
-	index := 0
+type Roman struct {
+	Value        int
+	Presentation string
+}
+
+func intToRoman(num int) string {
+	res := ""
+	romans := []Roman{
+		Roman{Value: 1, Presentation: "I"},
+		Roman{Value: 4, Presentation: "IV"},
+		Roman{Value: 5, Presentation: "V"},
+		Roman{Value: 9, Presentation: "IX"},
+		Roman{Value: 10, Presentation: "X"},
+		Roman{Value: 40, Presentation: "XL"},
+		Roman{Value: 50, Presentation: "L"},
+		Roman{Value: 90, Presentation: "XC"},
+		Roman{Value: 100, Presentation: "C"},
+		Roman{Value: 400, Presentation: "CD"},
+		Roman{Value: 500, Presentation: "D"},
+		Roman{Value: 900, Presentation: "CM"},
+		Roman{Value: 1000, Presentation: "M"},
+	}
+
 	for num != 0 {
-		if num-values[index] >= 0 {
-			num -= values[index]
-			s += literals[index]
-		} else {
-			index++
+		for i := 12; i >= 0; i-- {
+			if num >= romans[i].Value {
+				res += romans[i].Presentation
+				num -= romans[i].Value
+				break
+			}
 		}
 	}
-	return
+	return res
 }
