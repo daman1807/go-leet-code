e8dfe4b1d3102e132fed1ac02edcd7e3e712a798
Daman <aroradaman@gmail.com>
2022-07-13 16:48:00 +0530

    binary-search

--- 704. Binary Search/binary-search.go
+++ 704. Binary Search/binary-search.go
@@ -3,21 +3,18 @@ package main
 // https://leetcode.com/problems/binary-search/
 
 func search(nums []int, target int) int {
-	res := -1
+	var left, mid, right int
+	left, right = 0, len(nums)-1
 
-	low, mid, high := 0, 0, len(nums)-1
-
-	for low <= high {
-		mid = (high + low) / 2
-
+	for left <= right {
+		mid = (left + right) / 2
 		if nums[mid] == target {
-			res = mid
-			break
-		} else if nums[mid] < target {
-			low = mid + 1
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
 		} else {
-			high = mid - 1
+			left = mid + 1
 		}
 	}
-	return res
+	return -1
 }
